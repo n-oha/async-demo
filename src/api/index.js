ed9ae131977dc5d3f5757ad72dd01206c05e197b
Ohara Nobuaki <96947363+n-oha@users.noreply.github.com>
2022-02-05 17:19:56 +0900

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -2,10 +2,9 @@ import axios from 'axios'
 
 export const fetchData = async () => {          //async function でtry内の詳細を基にして指定のurlからデータを持ってくる様に指示してます
     try {
-        const data = {
+       const data = {
             method: 'get',
-            url: 'https://covid-19-data.p.rapidapi.com/country/code',
-            params: { code: "it" },
+            url: 'https://covid-19-data.p.rapidapi.com/totals',
             headers: {
                 'x-rapidapi-host': 'covid-19-data.p.rapidapi.com',
                 'x-rapidapi-key': 'aa4ac8ce04msh9a6283c48f5d93fp19a3a5jsnacb4dd1a5c68'
