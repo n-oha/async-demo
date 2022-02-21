ba7f5124819e85598075be791665ea635d948cab
Ohara Nobuaki <96947363+n-oha@users.noreply.github.com>
2022-02-22 00:33:22 +0900

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -4,10 +4,9 @@ export const fetchData = async () => {          //async function でtry内の詳
     try {
        const data = {
             method: 'get',
-            url: 'https://covid-19-data.p.rapidapi.com/totals',
+            url: '',
             headers: {
-                'x-rapidapi-host': 'covid-19-data.p.rapidapi.com',
-                'x-rapidapi-key': 'aa4ac8ce04msh9a6283c48f5d93fp19a3a5jsnacb4dd1a5c68'
+                
             }
         }
 
